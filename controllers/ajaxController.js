44f0642d5d3ee0b9a8c8317f11b69839dd05809b
Nguyen Van Hieu <elpulga246@gmail.com>
2020-01-05 12:30:37 +0700

    + phân trang tài khoản

---
+++ controllers/ajaxController.js
@@ -0,0 +1,32 @@
+const User = require('../models/user');
+const PAGE_SIZE = 10;
+const Handlebars = require('../helpers/handlebars');
+module.exports.getAccountPage = async(req, res, next) => {
+    let page = 1;
+    if (req.query.p) page = req.query.p;
+    let pageCount = Math.ceil(await User.getAllUserCount() / PAGE_SIZE);
+    let account = await User.getUserAtPage(page, PAGE_SIZE);
+    for (const item of account) {
+        if (item.id != null) {
+            if (item.authen == "1")
+                item.author = "Quản trị viên";
+            else if (item.authen == "0") item.author = "Khách hàng";
+            else item.author = "Chưa phân quyền";
+            const buy = await Order.getBuy(item.id);
+            item.buy = buy;
+        }
+    };
+    res.render('block-layout/account-block', {
+        title: 'Quản lí các tài khoản',
+        user: req.user,
+        accounts: account,
+        pagination: {
+            page: page,
+            pageCount: pageCount,
+            limit: 7
+        },
+        layout: 'block-layout/account-block'
+    }, (err, html) => {
+        res.send(html);
+    });
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,33 @@
 var User = require('../models/user');
 var Order = require('../models/order');
-
+const PAGE_SIZE = 10;
 const dashboardController = require('./dashboardControllers');
 
 module.exports.getAllUserInfo = async(req, res, next) => {
     if (!req.user) dashboardController.login(req, res, next);
     else {
         try {
-            const userinfo = await User.getAllUser();
-            for (const item of userinfo)
-            {
-                if(item.id!=null)
-                {
+            const userinfo = await User.getUserAtPage(1, PAGE_SIZE);
+            for (const item of userinfo) {
+                if (item.id != null) {
                     if (item.authen == "1")
-                    item.author = "Quản trị viên";
+                        item.author = "Quản trị viên";
                     else if (item.authen == "0") item.author = "Khách hàng";
                     else item.author = "Chưa phân quyền";
                     const buy = await Order.getBuy(item.id);
                     item.buy = buy;
                 }
             };
+            let count = await User.getAllUserCount();
             res.render('tai-khoan', {
                 title: 'Quản lí các tài khoản',
                 user: req.user,
-                accounts: userinfo
+                accounts: userinfo,
+                pagination: {
+                    page: 1,
+                    pageCount: Math.ceil(count / PAGE_SIZE),
+                    limit: 7
+                }
             });
         } catch (err) {
             next();
